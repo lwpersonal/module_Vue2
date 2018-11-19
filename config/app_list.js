ae441b79f3f2dce390cafc315b8b79ab9ce3e834
WenJW <wenjiawei@innobuddy.com>
2018-11-19 14:44:34 +0800

    入口配置

---
+++ config/app_list.js
@@ -0,0 +1,12 @@
+'use strict'
+// pages 目录下
+module.exports = {
+  app1: {
+    html: 'app1/index.html',
+    js: 'app1/index.js'
+  },
+  app2: {
+    html: 'app2/index.html',
+    js: 'app2/index.js'
+  }
+}
